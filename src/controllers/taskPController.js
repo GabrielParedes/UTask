bbbba6543832697801c1f4f0ac529e5d80e2e056
GabrielParedes <gparedes13092000@gmail.com>
2018-07-25 18:49:05 -0600

    .

--- src/controllers/taskPController.js
+++ src/controllers/taskPController.js
@@ -31,7 +31,7 @@ function createTaskP(req, res) {
 function readTaskP(req, res) {
   var userId = req.user.sub;
 
-  TaskP.findOne(userId, (err, taskPRead) => {
+  TaskP.find(userId, (err, taskPRead) => {
 
     if (err) return res.status(500).send(err)
 
